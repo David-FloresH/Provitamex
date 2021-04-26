7cc2681848ceeee17ca146aed194089e92c2bbbe
manuel <manuel_montoya97@hotmail.com>
2021-04-25 20:16:11 -0700

    sdfs

--- js/forms.js
+++ js/forms.js
@@ -1,1 +1,9 @@
-console.log("initial push");
+var app = angular.module('myApp', []);
+
+console.log("ya cargo");
+
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.message = function () {
+        console.log("I am in bro");
+    }
+  }]);
