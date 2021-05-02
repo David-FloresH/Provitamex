062cf669916d043af4897fb7f46a0e7e7116683c
manuel <manuel_montoya97@hotmail.com>
2021-05-01 17:18:22 -0700

    dfsdf

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ app.controller("MainController", ['$scope', function($scope) {
         "timeout": 0,
         "headers": {
           "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImthcmVuMXw3MjE4MSIsIkludGVybmFsSUQiOiI1Njc0OWE5Yy04ZTdjLTQ4YTktYjkxYS0wMjkyN2U3MGY4MDIiLCJuYmYiOjE2MTkzOTc2ODUsImV4cCI6MTY1MDkzMzY4NSwiaWF0IjoxNjE5Mzk3Njg1LCJpc3MiOiJNaW5udF9Tb2x1dGlvbnNfU0FfREVfQ1YiLCJhdWQiOiJCaW5kX0VSUF9BUElfVXNlcnMifQ.wjhxCvaXIv6A0Y2ook8dPdGF7WOHqf-i_vzoDJPTxQg",
-          "Cache-Control": "no-cache"
+          "Cache-Control": "no-cache",
+          "Access-Control-Allow-Origin":"http://192.168.0.2:3036/"
         },
       };
       
