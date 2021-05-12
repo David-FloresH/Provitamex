1199f6e9560112cf8c00854712fd07ed61f918f3
David Flores <david.isaacfloresh@gmail.com>
2021-05-11 22:37:46 -0700

    Allowed CrossOrigin Requests

--- Provitamex Bind Interface/website/src/main/java/com/provitamex/website/controller/MainController.java
+++ Provitamex Bind Interface/website/src/main/java/com/provitamex/website/controller/MainController.java
@@ -3,6 +3,7 @@ package com.provitamex.website.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,46 +24,60 @@ public class MainController {
 	@Autowired
 	MainService service;
 	
+	@CrossOrigin
 	@GetMapping("/warehouses")
 	public List<Warehouse> getWarehouses(){
 		System.out.println("WAREHOUSE METHOD CALLED");
 		return service.getWarehouses();
 	}
 	
+	@CrossOrigin
 	@GetMapping("/orders")
 	public List<Order> getOrders(){
 		System.out.println("ORDERS METHOD CALLED");
 		return service.getOrders();
 	}
 	
+	@CrossOrigin
 	@GetMapping("/inventory/{warehouseId}/{pricelistId}")
 	public List<Product2> getProducts(@PathVariable String warehouseId, @PathVariable String pricelistId){
 		System.out.println("WAREHOUSE INVENTORY METHOD CALLED");
 		return service.getProductsInWarehouse(warehouseId,pricelistId);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/orders/{id}")
 	public OrderDetails getOrderDetails(@PathVariable String id){
 		System.out.println("ORDER DETAILS METHOD CALLED");
 		return service.getOrderDetails(id);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/clients")
 	public List<Client> getClients(){
 		System.out.println("CLIENT METHOD CALLED");
 		return service.getClients();
 	}
 	
+	@CrossOrigin
 	@GetMapping("/clients/{id}")
 	public ClientDetails getClientDetails(@PathVariable String id){
 		System.out.println("CLIENT DETAILS METHOD CALLED");
 		return service.getClientDetails(id);
 	}
 	
+	@CrossOrigin
 	@PostMapping("/newclient")
 	public String setNewClient() {
 		System.out.println("NEW CLIENT METHOD CALLED");
 		return service.setNewClient();
+	}
+	
+	@CrossOrigin
+	@PostMapping("/neworder")
+	public String setNewOrder() {
+		System.out.println("NEW ORDER METHOD CALLED");
+		return service.setNewOrder();
 	}
 	
 }
