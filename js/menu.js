862aca315bc462b7170113fc649067a146cde15b
David Flores <david.isaacfloresh@gmail.com>
2021-05-18 23:34:12 -0700

    Changes to new client request data

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,37 @@
 console.log("inicio");
-
-// $.ajax({
-//   method: 'POST',
-//   jsonp: 'callback',
-//   url: 'http://localhost:8081/newclient',
-//   contentType: "application/json; charset=utf-8",
-//   dataType: 'json',
-//   data: JSON.stringify({
-//     legalName: "TestUser",
-//     pricelistId: "4cda058c-2d0a-4635-a7b2-597de294afed",
-//     telephone: "6861880576",
-//     streetName: "Cagliari",
-//     exteriorNo: "2267",
-//     colonia: "Bilbao",
-//     zipCode: "21254" ,
-//     city: "Mexicali",
-//     state: "Baja California",
-//     interiorNo: "1"
-//   }),
-//   beforeSend: function(xhr,settings){
-//     //spinner show;
-//   },
-//   success: function(response){
-//     console.log(response);
-//   },
-//   error: function(xhr,status,errorThrown) {
-//     console.log("Error");
-//   },
-//   complete: function(xhr, status){
-//     //spinner hide;
-//   }
-// });
-
+/*
+$.ajax({
+  method: 'POST',
+  jsonp: 'callback',
+  url: 'http://localhost:8081/newclient',
+  contentType: "application/json; charset=utf-8",
+  dataType: 'json',
+  data: JSON.stringify({
+    legalName: "TestUser",
+    pricelistId: "4cda058c-2d0a-4635-a7b2-597de294afed",
+    telephone: "6861880576",
+    streetName: "Cagliari",
+    exteriorNo: "2267",
+    colonia: "Bilbao",
+    zipCode: "21254" ,
+    city: "Mexicali",
+    state: "Baja California",
+    interiorNo: "1"
+  }),
+  beforeSend: function(xhr,settings){
+    //spinner show;
+  },
+  success: function(response){
+    console.log(response);
+  },
+  error: function(xhr,status,errorThrown) {
+    console.log("Error");
+  },
+  complete: function(xhr, status){
+    //spinner hide;
+  }
+});
+*/
 var app = angular.module("myApp", []);
 var scope;
 
